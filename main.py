c08fc3d2650e147c58e76cfe8d6ec1848e878b4c
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-09-21 17:17:21 +0400

    Init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from selene import browser, have, be
+assert isinstance(browser, object)
+
+
+def test_login_successful():
+    # GIVEN
+    browser.open("https://demowebshop.tricentis.com/")
+    # WHEN
+    browser.element('.ico-login').click()
+    # AND
+    browser.element('#Email').type('yashaka@gmail.com')
+    browser.element('#Password').type("Pa$$w0rd")
+    browser.element('.login-button').click()
+    # THEN
+    browser.element('.header .account').should(have.exact_text('yashaka@gmail.com'))
+    browser.element('.ico-logout').should(be.visible)
+
+
+def test_logout_successful():
+    browser.element('')
